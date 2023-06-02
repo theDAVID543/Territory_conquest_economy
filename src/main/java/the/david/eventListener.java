31f4134c4c551a344a6c451911a5f32cdb481bb4
theDAVID543 <theDAVID5432@gmail.com>
2023-06-02 19:03:41 +0800

    update

--- src/main/java/the/david/eventListener.java
+++ src/main/java/the/david/eventListener.java
@@ -2,12 +2,20 @@ package the.david;
 
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
 import com.google.common.base.Objects;
+import io.papermc.paper.event.block.BlockBreakBlockEvent;
 import me.angeschossen.lands.api.LandsIntegration;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class eventListener implements Listener {
+    private List<Material> crops = Arrays.asList(new Material[]{Material.WHEAT, Material.COCOA, Material.BEETROOTS, Material.CARROTS, Material.POTATOES, Material.SWEET_BERRY_BUSH});
     private LandsIntegration landsAPI = Territory_conquest_economy.landsAPI;
     @EventHandler
     public void onPickExpOrb(PlayerPickupExperienceEvent e){
@@ -23,14 +31,32 @@ public class eventListener implements Listener {
                 ConfigReader.setLandExpConfig(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId(), e.getExperienceOrb().getExperience());
             }
         }
-        while(!Objects.equal(ConfigReader.playerExpGot(e.getPlayer().getUniqueId()), null) && ConfigReader.playerExpGot(e.getPlayer().getUniqueId()) >= 25){
-            Territory_conquest_economy.getEconomy().depositPlayer(e.getPlayer(),0.1);
-            ConfigReader.setPlayerExpConfig(e.getPlayer().getUniqueId(), ConfigReader.playerExpGot(e.getPlayer().getUniqueId()) - 25);
+        while(!Objects.equal(ConfigReader.playerExpGot(e.getPlayer().getUniqueId()), null) && ConfigReader.playerExpGot(e.getPlayer().getUniqueId()) >= 1){
+            Territory_conquest_economy.getEconomy().depositPlayer(e.getPlayer(),0.01);
+            ConfigReader.setPlayerExpConfig(e.getPlayer().getUniqueId(), ConfigReader.playerExpGot(e.getPlayer().getUniqueId()) - 1);
         }
-        while(!Objects.equal(ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()), null) && ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()) >= 125 - ((landsAPI.getArea(e.getPlayer().getLocation()).getLand().getLevel().getPosition() + 1) * 10 )){
-            landsAPI.getArea(e.getPlayer().getLocation()).getLand().addBalance(0.1);
-            ConfigReader.setLandExpConfig(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId(), ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()) - 125 - ((landsAPI.getArea(e.getPlayer().getLocation()).getLand().getLevel().getPosition() + 1) * 10 ) );
+        while(!Objects.equal(ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()), null) && ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()) >= 25 - ((landsAPI.getArea(e.getPlayer().getLocation()).getLand().getLevel().getPosition() - 1) * 2 )){
+            landsAPI.getArea(e.getPlayer().getLocation()).getLand().addBalance(0.01);
+            ConfigReader.setLandExpConfig(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId(), ConfigReader.landExpGot(landsAPI.getArea(e.getPlayer().getLocation()).getLand().getId()) - 25 - ((landsAPI.getArea(e.getPlayer().getLocation()).getLand().getLevel().getPosition() - 1) * 2 ) );
             landsAPI.getArea(e.getPlayer().getLocation()).getLand();
+        }
+    }
+    @EventHandler
+    public void onBlockBreakBlock(BlockBreakBlockEvent e){
+        if(crops.contains(e.getBlock().getType())){
+            Ageable crop = (Ageable) e.getBlock().getBlockData();
+            if(crop.getAge() == crop.getMaximumAge()){
+                Bukkit.getLogger().info("grown");
+            }
+        }
+    }
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent e){
+        if(crops.contains(e.getBlock().getType())){
+            Ageable crop = (Ageable) e.getBlock().getBlockData();
+            if(crop.getAge() == crop.getMaximumAge()){
+                Bukkit.getLogger().info("grown");
+            }
         }
     }
 }
